a4fcf04c84d3178e6cfde60793cd43965b38a69a
Aydan <148878657+AydanL@users.noreply.github.com>
2024-04-16 12:20:55 +0200

    Delete projet_final/code/game.py

--- projet_final/code/game.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from player import Player
-# classe du jeu 
-class Game:
-    def __init__(self):
-        #chargement du joueur
-        self.player = Player()
-        self.pressed = {}
