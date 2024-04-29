8bf2a7d0ab627d8549d411763639246e18b3f885
ilyasdevelop <69214811+ilyasdevelop@users.noreply.github.com>
2024-04-29 19:55:28 +0200

    Update textures.py

--- tests divers/textures.py
+++ tests divers/textures.py
@@ -5,12 +5,7 @@ import pygame
 def frame(pic, x, frame_width=64):
     frame = pic.subsurface(pygame.Rect( frame_width * (x - 1), 0, frame_width, frame_width ))
     return frame
-#bouton play 
-button_play = pygame.image.load("projet_final/textures/misc/button_play.png")
-button_play = pygame.transform.scale(button_play, (200, 100))
-button_play_rect = button_play.get_rect()
-button_play_rect = button_play_rect.move(820, 400)
-button_play_rect = button_play.get_rect()
+
 # import du vrai bouton play
 button_Play = pygame.image.load("projet_final/textures/misc/Play.png")
 button_Play = pygame.transform.scale(button_Play, (button_Play.get_width() * 3, button_Play.get_height() * 3))
@@ -19,24 +14,25 @@ button_play_on = button_Play.subsurface((0, 0, width_button_play // 2, height_bu
 button_play_off = button_Play.subsurface((width_button_play // 2, 0, width_button_play // 2, height_button_play))
 button_play_on_rect = button_play_on.get_rect()
 button_play_on_rect = button_play_on_rect.move(820, 400)
-'''
-button_Play = pygame.image.load("projet_final/textures/misc/Play.png")
-height_button_Play, width_button_Play = button_Play.get_size()
-button_Play_on = button_Play.subsurface(pygame.Rect(0, 0, width_button_Play // 2, height_button_Play))
-button_Play_off = button_Play.subsurface(pygame.Rect(width_button_Play // 2, 0, width_button_Play // 2, height_button_Play))
-button_go = {"on":button_Play_on,"off":button_Play_off}
-button_Play = {}
-button_Play["mms"] = pygame.image.load("projet_final/textures/misc/Play.png")
-button_Play["mms"]  = pygame.transform.scale(button_Play["mms"], (button_Play["mms"].get_width() * 2, button_Play["mms"].get_height() * 2))
-button_Play["off"] = frame(button_Play["mms"], 1)
-button_Play["on"] = frame(button_Play["mms"], 2)
-'''
-#bouton son 
+
+#bouton son yes
 button_sound = pygame.image.load("projet_final/textures/misc/Bouton.png")
+button_sound = pygame.transform.scale(button_sound, (button_sound.get_width() * 2, button_sound.get_height() * 2))
+width_button_sound, height_button_sound = button_sound.get_size()
+button_sound_off = button_sound.subsurface((0, 0, width_button_sound // 2, height_button_sound))
+button_sound_on = button_sound.subsurface((width_button_sound // 2, 0, width_button_sound // 2, height_button_sound))
+button_sound_off_rect = button_sound_off.get_rect()
+button_sound_off_rect = button_sound_off_rect.move(230,420)
+#bouton son no
+button_sound_no = pygame.image.load("projet_final/textures/misc/Bouton.png")
+button_sound_no = pygame.transform.scale(button_sound_no, (button_sound_no.get_width() * 2, button_sound_no.get_height() * 2))
+width_button_sound_no, height_button_sound_no = button_sound_no.get_size()
+button_sound_no_off = button_sound_no.subsurface((0, 0, width_button_sound_no // 2, height_button_sound_no))
+button_sound_no_on = button_sound_no.subsurface((width_button_sound_no // 2, 0, width_button_sound_no // 2, height_button_sound_no))
 #logo
 logo = pygame.image.load('projet_final/textures/misc/Logo.png')
 #perso 
-theme = "chevalier"
+theme = "SUSHI"
 player = {}
 player["bitmap"]  = pygame.image.load('projet_final/textures/player/perso_'+theme+'_run.png')
 player["bitmap"]  = pygame.transform.scale(player["bitmap"], (player["bitmap"].get_width() * 2, player["bitmap"].get_height() * 2))
