cfa893f3d65c8b5bda38d70234e4fe4ef450fa28
ilyasdevelop <69214811+ilyasdevelop@users.noreply.github.com>
2024-04-30 08:39:05 +0200

    Update textures.py

--- tests divers/textures.py
+++ tests divers/textures.py
@@ -19,18 +19,26 @@ button_play_on_rect = button_play_on_rect.move(820, 400)
 button_sound = pygame.image.load("projet_final/textures/misc/Bouton.png")
 button_sound = pygame.transform.scale(button_sound, (button_sound.get_width() * 2, button_sound.get_height() * 2))
 width_button_sound, height_button_sound = button_sound.get_size()
-button_sound_off = button_sound.subsurface((0, 0, width_button_sound // 2, height_button_sound))
-button_sound_on = button_sound.subsurface((width_button_sound // 2, 0, width_button_sound // 2, height_button_sound))
-button_sound_off_rect = button_sound_off.get_rect()
-button_sound_off_rect = button_sound_off_rect.move(230,420)
+button_sound_yes_off = button_sound.subsurface((0, 0, width_button_sound // 2, height_button_sound))
+button_sound_yes_on = button_sound.subsurface((width_button_sound // 2, 0, width_button_sound // 2, height_button_sound))
+button_sound_yes_rect = button_sound_yes_off.get_rect()
+button_sound_yes_rect = button_sound_yes_rect.move(230,420)
+
+
 #bouton son no
 button_sound_no = pygame.image.load("projet_final/textures/misc/Bouton.png")
 button_sound_no = pygame.transform.scale(button_sound_no, (button_sound_no.get_width() * 2, button_sound_no.get_height() * 2))
 width_button_sound_no, height_button_sound_no = button_sound_no.get_size()
 button_sound_no_off = button_sound_no.subsurface((0, 0, width_button_sound_no // 2, height_button_sound_no))
 button_sound_no_on = button_sound_no.subsurface((width_button_sound_no // 2, 0, width_button_sound_no // 2, height_button_sound_no))
+button_sound_no_rect = button_sound_no_off.get_rect()
+button_sound_no_rect = button_sound_no_rect.move(343, 420)
+
+
 #logo
 logo = pygame.image.load('projet_final/textures/misc/Logo.png')
+
+
 #perso 
 theme = "SUSHI"
 player = {}
